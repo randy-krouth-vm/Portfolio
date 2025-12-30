3122c7b5c56172258a08b3c10019f6abe5b3334c
randy-krouth-vm <randy.krouth.vm@outlook.com>
2025-12-30 00:21:37 -0500

    Fixed mobile thumbnail issue.

--- portfolio/src/Previews/Previews.jsx
+++ portfolio/src/Previews/Previews.jsx
@@ -6,37 +6,29 @@ function Previews() {
     return (
     <>
         <div className="video-wrapper">
-            <video controls>
-                <source src={`${import.meta.env.BASE_URL}Recording_1.mp4`} type="video/mp4"
-                poster={`${import.meta.env.BASE_URL}Recording_1.png`}/>
-
+            <video controls preload="none" playsInline poster={`${import.meta.env.BASE_URL}Recording_1.png`}>
+                <source src={`${import.meta.env.BASE_URL}Recording_1.mp4`} type="video/mp4"/>
             </video>
 
-            <video controls>
-                <source src={`${import.meta.env.BASE_URL}Recording_2.mp4`} type="video/mp4"
-                poster={`${import.meta.env.BASE_URL}Recording_2.png`}/>
+            <video controls preload="none" playsInline poster={`${import.meta.env.BASE_URL}Recording_2.png`}>
+                <source src={`${import.meta.env.BASE_URL}Recording_2.mp4`} type="video/mp4"/>
             </video>
 
-            <video controls>
-                <source src={`${import.meta.env.BASE_URL}Recording_3.mp4`} type="video/mp4"
-                poster={`${import.meta.env.BASE_URL}Recording_3.png`}/>
+            <video controls preload="none" playsInline poster={`${import.meta.env.BASE_URL}Recording_3.png`}>
+                <source src={`${import.meta.env.BASE_URL}Recording_3.mp4`} type="video/mp4"/>
             </video>
 
-            <video controls>
-                <source src={`${import.meta.env.BASE_URL}Final_Project_Unreal.mp4`} type="video/mp4"
-                    poster={`${import.meta.env.BASE_URL}Final_Project_Unreal_Poster.png`}/>
+            <video controls preload="none" playsInline poster={`${import.meta.env.BASE_URL}Final_Project_Unreal_Poster.png`}>
+                <source src={`${import.meta.env.BASE_URL}Final_Project_Unreal.mp4`} type="video/mp4"/>
             </video>
 
-            <video controls>
-                <source src={`${import.meta.env.BASE_URL}AutoFit_Example.mp4`} type="video/mp4"
-                    poster={`${import.meta.env.BASE_URL}AutoFit_Example_Poster.png`}/>
+            <video controls preload="none" playsInline poster={`${import.meta.env.BASE_URL}AutoFit_Example_Poster.png`}>
+                <source src={`${import.meta.env.BASE_URL}AutoFit_Example.mp4`} type="video/mp4"/>
             </video>
 
-            <video controls>
-                <source src={`${import.meta.env.BASE_URL}Scheduler_Example.mp4`} type="video/mp4"
-                    poster={`${import.meta.env.BASE_URL}Scheduler_Example_Poster.png`}/>
+            <video controls preload="none" playsInline poster={`${import.meta.env.BASE_URL}Scheduler_Example_Poster.png`}>
+                <source src={`${import.meta.env.BASE_URL}Scheduler_Example.mp4`} type="video/mp4"/>
             </video>
-
         </div>
     </>
     );
