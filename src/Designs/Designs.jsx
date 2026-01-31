d3e83d49dc203cf62d2043261f28a3c4f32be9b0
randall-krouth <randy.krouth@gmail.com>
2026-01-31 00:12:07 -0500

    Added encryption model.

--- src/Designs/Designs.jsx
+++ src/Designs/Designs.jsx
@@ -8,12 +8,12 @@ function Designs() {
         <div className="designs-container">
             <p>CI/CD:</p>
             <img src="Architectural_Design_3.png"/>
-            <p>Event Driven Streaming:</p>
-            <img src="Architectural_Design_2.png"/>
             <p>Generic Web Architecture:</p>
             <img src="Architectural_Design_1.png" />
             <p>Industry Standard:</p>
             <img src="Industry_Standard_Design.png" />
+            <p>Encryption Guidelines:</p>
+            <img src="Architectural_Design_4.png" />
         </div>
     </>
   );
