02b03ca8f055168fcc37169fefea4b1890eb015e
randall-krouth <randy.krouth@gmail.com>
2026-02-26 02:34:27 -0500

    Added design.

--- src/Designs/Designs.jsx
+++ src/Designs/Designs.jsx
@@ -18,6 +18,8 @@ function Designs() {
             <img src="Industry_Standard_Serverless_Design_With_Security.png" />
             <p>Encryption Guidelines:</p>
             <img src="Architectural_Design_4.png" />
+            <p>Authentication Identity Control Layer:</p>
+            <img src="Authentication_Control_Layer.png" />
             <p>Security Baseline:</p>
             <img src="Security_Baseline.png" />
         </div>
