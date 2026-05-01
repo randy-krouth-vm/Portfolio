64421ec915c27a1d69a889fdcdf97a1a4ee22d6b
randall-krouth <randy.krouth@gmail.com>
2026-04-30 22:27:53 -0400

    Update.

--- src/Home/App.jsx
+++ src/Home/App.jsx
@@ -17,8 +17,8 @@ function App() {
           <ImageGrid imageOne="Image_3_Gray_and_White_Minimalist_Desktop_Mockup_Instagram_Post.png" imageOneText="Scalable Compute Applications"
               imageTwo="Image_4_Brown_and_Gray_Minimalist_Desktop_Mockup_Instagram_Post.png" imageTwoText="Interactive Game Design" />
           <SectionThree />
-          <ImageGrid imageOne="Image_8_Black_&_White_Modern_QR_Code_Wifi_Access_Flyer.png" imageOneText="Identity Access Management (IAM)"
-              imageTwo="Image_9_Cloud_Server_Logo.png" imageTwoText="Back-End Development" />
+          <ImageGrid imageOne="Keycloak.png" imageOneText="Identity Access Management (IAM)"
+              imageTwo="Terminal.png" imageTwoText="Back-End Development" />
           <BottomSection />
     </>
   )
